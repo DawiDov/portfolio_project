965c6efd3ab389c74d2e1fa93da43d3f5becd8c0
Артур Давидов <art@mac.local>
2022-06-14 17:51:54 +0300

    sticky header

--- frontend/src/components/layoutComponents/Header/index.jsx
+++ frontend/src/components/layoutComponents/Header/index.jsx
@@ -3,25 +3,27 @@ import style from './index.module.sass'
 
 const Header = () => (
   <header className={style.header}>
-    <nav className={style.menu}>
-      <ul>
-        <li>
-          <a href='#home'>Home</a>
-        </li>
-        <li>
-          <a href='#about'>About</a>
-        </li>
-        <li>
-          <a href='#resume'>Resume</a>
-        </li>
-        <li>
-          <a href='#works'>Works</a>
-        </li>
-        <li>
-          <a href='#contact'>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div className={style.menuWrapper}>
+      <nav className={style.menu}>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#resume'>Resume</a>
+          </li>
+          <li>
+            <a href='#works'>Works</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 )
 
