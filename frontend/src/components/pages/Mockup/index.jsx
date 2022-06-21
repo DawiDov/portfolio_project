dfdb3802d921894146a81db38d12c0fd7d2bd21f
Artur Davidov <79329347+DawiDov@users.noreply.github.com>
2022-06-21 19:21:19 +0300

    mockup page (#54)
    * fixed container component
    
    * mockup page
    
    * mockup page
    
    Co-authored-by: Damir Akhmadullin <damir@damir.local>
    Co-authored-by: Артур Давидов <art@mac.local>

---
+++ frontend/src/components/pages/Mockup/index.jsx
@@ -0,0 +1,10 @@
+import style from './index.module.sass'
+
+const Mockup = () => (
+  <section className={ style.container }>
+    <h1>Станица не найдена</h1>
+    <h2>Попоробуйте указать имя кандидата в URL</h2>
+  </section>
+)
+
+export default Mockup
