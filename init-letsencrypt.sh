a68029fae2e8df722d7cc1d5c8d98b71d5f0eb55
Artur Davidov <79329347+DawiDov@users.noreply.github.com>
2022-06-23 19:28:26 +0300

    Update domains in init-letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example_domain.test www.example_domain.test)
+domains=(кандидат.рус)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
