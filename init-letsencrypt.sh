bd8b19b83ba4eff63802f8395c40a664cf0786e9
Damir Akhmadullin <damir@damir.local>
2022-06-24 13:28:04 +0300

    заменить домен example на свой

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example_domain.test www.example_domain.test)
+domains=(pet-project.site)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
