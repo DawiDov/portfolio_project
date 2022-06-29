19bba8940811f5261ae1a30880d1661dcaac46ac
Artur Davidov <79329347+DawiDov@users.noreply.github.com>
2022-06-29 14:41:09 +0300

    Mockup fix (#100)
    * fixed container component
    
    * dinamic url gitButton
    
    * git button dynamic link
    
    * pre deploy
    
    * fix domain
    
    * fix mockup and gitbutton
    
    * remove comment
    
    * fix adapt title and github button
    
    Co-authored-by: Damir Akhmadullin <damir@damir.local>
    Co-authored-by: Артур Давидов <art@mac.local>

--- frontend/src/components/pages/Mockup/index.jsx
+++ frontend/src/components/pages/Mockup/index.jsx
@@ -2,8 +2,8 @@ import style from './index.module.sass'
 
 const Mockup = () => (
   <section className={ style.container }>
-    <h1>Станица не найдена</h1>
-    <h2>Попоробуйте указать имя кандидата в URL</h2>
+    <p>Станица не найдена</p>
+    <p>Попоробуйте указать имя кандидата в URL</p>
   </section>
 )
 
