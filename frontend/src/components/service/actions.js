da243581d34baa01aa15676ae94033749dc5c2be
Damir Akhmadullin <damir@damir.local>
2022-06-29 18:03:57 +0300

    changed server request

--- frontend/src/components/service/actions.js
+++ frontend/src/components/service/actions.js
@@ -15,7 +15,7 @@ const getData = (candidateName) => (dispatch) => {
 
   axios.defaults.baseURL = `${window.location.origin}/`
 
-  const link = `http://151.248.115.145/api/v1${candidateName}`
+  const link = `https://151.248.115.145/api/v1${candidateName}`
   // const link = `http://localhost/api/v1${candidateName}`
 
   axios.get(link).then(resp => {
