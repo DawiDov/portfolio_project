611c2dfd27c956e075709722af28acba8eb0a735
Артур Давидов <art@mac.local>
2022-06-30 19:14:15 +0300

    added www. domain in letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(pet-project.site)
+domains=(pet-project.site www.pet-project.site)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
