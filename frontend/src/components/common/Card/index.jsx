e753c3494d458d686fd0b31d158b4ce7b5bd60ca
Damir Akhmadullin <damir@damir.local>
2022-07-04 15:44:06 +0300

    card + home

--- frontend/src/components/common/Card/index.jsx
+++ frontend/src/components/common/Card/index.jsx
@@ -5,7 +5,7 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
 
 import style from './index.module.sass'
 
-const Card = ({ value, icon, className }) => (
+const Card = ({ isLink, title, value, icon, className }) => (
 
   <article
     className={
@@ -14,19 +14,36 @@ const Card = ({ value, icon, className }) => (
     <picture className={style.icon}>
       {icon}
     </picture>
-    <address className={style.value}>
-      {value}
+    <address className={style.address}>
+      {isLink ? (
+        <a
+          className={style.link}
+          rel='noreferrer'
+          target='_blank'
+          href={value}>
+          {title}
+        </a>
+      ) : (
+        <div
+          className={style.link}
+        >
+          {title}
+        </div>)
+      }
     </address>
   </article>
 )
 
 Card.propTypes = {
+  isLink: PropTypes.bool,
   value: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   icon: PropTypes.node,
   className: PropTypes.string,
 }
 
 Card.defaultProps = {
+  isLink: false,
   className: null,
   icon: <LocalPhoneIcon />
 }
