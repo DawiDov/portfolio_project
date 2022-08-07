b522db32e42341c8adc1eba223cbb6e09377cc30
Damir Akhmadullin <damir@damir.local>
2022-08-07 12:11:44 +0300

    move pages folder to src

--- frontend/src/pages/App/index.jsx
+++ frontend/src/pages/App/index.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 
 import Layout from 'components/layoutComponents/Layout'
-import PageNotFound from 'components/pages/PageNotFound'
-import Home from 'components/pages/Home'
-import Mockup from 'components/pages/Mockup'
+import PageNotFound from 'pages/PageNotFound'
+import Home from 'pages/Home'
+import Mockup from 'pages/Mockup'
 
 const App = () => (
   <Routes>
