51c9bc43d3cf644ad5b0444c17717626f63a1a17
Damir Akhmadullin <damir@damir.local>
2022-08-07 12:50:33 +0300

    deleted layoutComponents

--- frontend/src/pages/App/index.jsx
+++ frontend/src/pages/App/index.jsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import React from 'react'
 
-import Layout from 'components/layoutComponents/Layout'
 import PageNotFound from 'pages/PageNotFound'
 import Home from 'pages/Home'
 import Mockup from 'pages/Mockup'
+import { ThemeProvider } from '@emotion/react'
+import theme from 'style'
 
 const App = () => (
-  <Routes>
-    <Route path='/' element={<Layout />} >
-      <Route index element={<Mockup />} />
+  <ThemeProvider theme={theme}>
+    <Routes>
+      <Route ipath='/' element={<Mockup />} />
       <Route path={encodeURIComponent('ADavidov')} element={<Home />} />
       <Route path={encodeURIComponent('DAkhmadullin')} element={<Home />} />
-    </Route>
-    <Route path='*' element={<PageNotFound />} />
-  </Routes>
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+  </ThemeProvider>
 )
 
 
